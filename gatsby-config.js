9b4b320dee72a1931a94352a8df10e0964341444
Mike Joyce <michael.joyce@target.com>
2019-01-19 06:11:03 -1000

    add GA id

--- gatsby-config.js
+++ gatsby-config.js
@@ -50,7 +50,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: 'UA-104564811-3',
       },
     },
     `gatsby-plugin-feed`,
