06a32cd19f1f38aa6c1dd106a0a37dfb539bfd4d
Mike Joyce <michael.joyce@target.com>
2019-01-22 20:26:13 -0600

    add external links plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,6 +24,13 @@ module.exports = {
       options: {
         plugins: [
           {
+            resolve: 'gatsby-remark-external-links',
+            options: {
+              target: '_blank',
+              rel: 'nofollow',
+            },
+          },
+          {
             resolve: `gatsby-remark-images`,
             options: {
               maxWidth: 590,
